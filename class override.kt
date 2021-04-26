63b8619364a2461cae46ea4bad9ff410af6dcbf0
donghyun3996 <81354005+donghyun3996@users.noreply.github.com>
2021-04-26 23:32:47 +0900

    Create class override.kt

---
+++ class override.kt
@@ -0,0 +1,73 @@
+fun main() {
+	
+    //오버라이딩
+    /*var t =Tiger()
+    
+    t.eat()
+    
+}
+
+open class Animal {
+   open fun eat(){
+        println("음식을 먹습니다")
+    }
+}
+class Tiger : Animal(){
+    override fun eat(){
+        println ("고기를 먹습니다")
+    }*/
+    
+    
+    
+    //추상함수
+  /* 
+    var r = Rabbit()
+    
+    r.eat()
+    r.sniff()
+    
+    
+}
+abstract class Animal {
+    abstract fun eat()
+    fun sniff(){
+        println("킁킁")
+    }
+}
+
+class Rabbit : Animal(){
+    override fun eat(){
+        println("당근을 흔들어요.")
+    }*/
+    
+  //인터페이스
+  /*  
+    var d = Dog()
+    
+    d.run()
+    d.eat()
+    
+    
+    
+    
+}
+
+
+interface Runner {
+    fun run()
+}
+interface Eater{
+    fun eat(){
+        println("음식을 먹습니다.")
+    }
+}
+
+class Dog : Runner,Eater{
+    override fun run(){
+        println("우다다다 걸읍니다")
+        
+    }
+    override fun eat(){
+        println("허겁지겁 먹어.")
+    }*/
+}
