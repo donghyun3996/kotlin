7ffe76a8c08a2d0338cec26f89978af748a24d5a
donghyun3996 <81354005+donghyun3996@users.noreply.github.com>
2021-04-27 20:07:40 +0900

    Create scope.kt

---
+++ scope.kt
@@ -0,0 +1,29 @@
+//스코프
+
+val a = "패키지 코틀린"
+
+class B {
+    val a = "클래스 스코프"
+    
+    fun print(){
+        println(a)
+    }
+}
+fun main() {
+val a = "함수 스코프"
+ println(a)
+ B().print()
+    
+}
+
+//접근제한자
+
+//패키지 스코프에서
+//public 어떤 패키지에서도 접근 가능
+//internal 같은 모듈 내에서만 접근 가능
+//private 같은 파일 내에서만 접근가능
+
+//클래스 스코프에서
+//public 클래스 외부에서 늘 접근가능
+//private 클래스 내부레서만 접근가능
+//protected 클래스 자신과 상속받은 클래스에서 접근 가능
